6b20b8b9dd0e920a1b6d307f5e6dc08535fdc5eb
Lucie Klimesova <xklimes4@fi.muni.cz>
2022-06-15 20:38:11 +0200

    feat: add and remove textfileds for step and ingrediences

--- sources/frontend/src/components/AddRecipe.tsx
+++ sources/frontend/src/components/AddRecipe.tsx
@@ -11,6 +11,7 @@ import Tooltip from '@mui/material/Tooltip';
 import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
 import RefreshIcon from '@mui/icons-material/Refresh';
+import DeleteIcon from '@mui/icons-material/Delete';
 import { styled } from '@mui/material/styles';
 import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
@@ -22,9 +23,27 @@ const Input = styled('input')({
 export const AddRecipe = () => {
   const [stepCount, setStepCount] = useState(1);
 
-  const [inputFields, setInputFields] = useState([
+  const [stepFields, setStepFields] = useState([
     { id: uuidv4(), step: "" },
   ]);
+
+  const removeStepFields = (id: any) => {
+    const values  = [...stepFields];
+    console.log(id);
+    values.splice(values.findIndex(value => value.id === id), 1);
+    setStepFields(values);
+  }
+
+  const [ingrediceFields, setIngrediceFields] = useState([
+    { id: uuidv4(), ingredice: "" },
+  ]);
+
+  const removeIngrediceFields = (id: any) => {
+    const values  = [...ingrediceFields];
+    console.log(id);
+    values.splice(values.findIndex(value => value.id === id), 1);
+    setIngrediceFields(values);
+  }
 
   return (
     <Grid container direction="column">
@@ -36,7 +55,7 @@ export const AddRecipe = () => {
         label="Name"
         margin="dense"
       />
-      <Grid container spacing={2}>
+      <Grid container spacing={2} alignItems="center">
         <Grid item>
           <TextField
             id="filled-basic"
@@ -54,7 +73,7 @@ export const AddRecipe = () => {
         </Grid>
 
         <Grid item>
-          <Grid container spacing={2}>
+          <Grid container spacing={2} alignItems="center">
             <Grid item>
               <label htmlFor="contained-button-file">
                 <Input accept="image/*" id="contained-button-file" multiple type="file" />
@@ -73,22 +92,51 @@ export const AddRecipe = () => {
         </Grid>
       </Grid>
       
-      <Typography color="text.primary">
-        Steps:
-      </Typography>
-      <Grid>
-        <Grid>
-          {inputFields.map(inputField => (
+      <Grid item>
+        <Grid container spacing={2}>
+          <Grid item xs={4}>
+            <Typography color="text.primary">
+              Steps:
+            </Typography>
             <Grid>
-              <TextField id="standard-basic" value={inputField.step}/>
+              <Grid>
+                {stepFields.map(inputField => (
+                  <Grid container alignItems="center">
+                    <TextField
+                      id="standard-basic"
+                      margin="dense"/>
+                    <IconButton disabled={stepFields.length === 1} onClick={() => removeStepFields(inputField.id)}>
+                      <DeleteIcon />
+                    </IconButton>
+                  </Grid>
+                ))}
+              </Grid>
+              <IconButton onClick={() => setStepFields([...stepFields, { id: uuidv4(), step: "" }])}><AddIcon /></IconButton>
             </Grid>
-          ))}
+          </Grid>
+          
+          <Grid item xs={8}>
+            <Typography color="text.primary">
+              Ingrediences:
+            </Typography>
+            <Grid>
+              <Grid>
+                {ingrediceFields.map(inputField => (
+                  <Grid container alignItems="center">
+                    <TextField
+                      id="standard-basic"
+                      margin="dense"/>
+                    <IconButton disabled={ingrediceFields.length === 1} onClick={() => removeIngrediceFields(inputField.id)}>
+                      <DeleteIcon />
+                    </IconButton>
+                  </Grid>
+                ))}
+              </Grid>
+              <IconButton onClick={() => setIngrediceFields([...ingrediceFields, { id: uuidv4(), ingredice: "" }])}><AddIcon /></IconButton>
+            </Grid>
+          </Grid>
         </Grid>
-        <IconButton onClick={() => setInputFields([...inputFields, { id: uuidv4(), step: "" }])}><AddIcon /></IconButton>
       </Grid>
-      <Typography color="text.primary">
-        Ingrediences:
-      </Typography>
     </Grid>
   );
 }
