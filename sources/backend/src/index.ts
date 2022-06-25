7603a02750130890c8f9cddd117055e0fb07bbe7
Vojtěch Straka <vojtstraka@gmail.com>
2022-06-25 15:43:45 +0200

    Adding forgotten changes

--- sources/backend/src/index.ts
+++ sources/backend/src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import { recipe, category } from './resources';
+import { recipe, category, image } from './resources';
 
 const api = express();
 
@@ -15,6 +15,7 @@ api.use(express.static('public'));
 api.get('/recipe',          recipe.list);
 api.get('/recipe/:id',      recipe.show);
 api.post('/recipe',         recipe.create);
+api.post('/recipe/:id/image',     image.upload)
 api.delete('/recipe/:id',   recipe.destroy);
 
 // Categories
@@ -25,4 +26,4 @@ api.get('/category/:id/name',   category.getCategoryName)
 /**
  * Start listening on connections
  */
-api.listen(process.env.REACT_APP_BACKEND_PORT, () => console.log(`Example app listening on port ${process.env.REACT_APP_BACKEND_PORT}`));
+api.listen(process.env.REACT_APP_BACKEND_PORT, () => console.log(`App listening on port ${process.env.REACT_APP_BACKEND_PORT}`));
--- sources/backend/src/resources/image.ts
+++ sources/backend/src/resources/image.ts
@@ -1,13 +1,29 @@
+import prisma from '../client';
 import { Request, Response } from 'express';
-import multer from 'multer';
+import { object, string } from 'yup';
 
-const imageUpload = multer({
-    dest: 'images',
-   });
-   
+const imageSchema = object({
+    base64: string().required(),
+    recipeId: string().required().uuid(),
+});
 
 export const upload = async (req: Request, res: Response) => {
-    console.log(req.file);
+    console.log(req.body);
+    console.log(Object.keys(req.body.data)[0]);
+    console.log(req.params.id);
+    const image = {
+        base64: Object.keys(req.body.data)[0],
+        recipeId: req.params.id!,
+    }
+    try {
+        const data = await imageSchema.validate(image);
+        await prisma.image.create({
+            data
+        })
+    } catch(e) {
+        //console.log(e)
+    }
+
 
     return res.send({
         status: 'success'
