293888c3416468d251a02417674218d459172973
Vojtěch Straka <vojtstraka@gmail.com>
2022-06-28 14:01:25 +0200

    Added image seving

--- sources/backend/src/index.ts
+++ sources/backend/src/index.ts
@@ -13,13 +13,14 @@ api.use(express.urlencoded({ extended: true }));
 api.use(express.static('public'));
 
 // Recipe handling
-api.get('/recipe',                recipe.list);
-api.get('/recipe/:id',            recipe.show);
-api.post('/recipe',               recipe.create);
-api.get('/recipe/:id/image',      recipe.getImages);
-api.post('/recipe/:id/image',     image.upload)
-api.post('/recipe/:id/image2',    multer().single('body'), image.upload)
-api.delete('/recipe/:id',         recipe.destroy);
+api.get('/recipe',                                              recipe.list);
+api.get('/recipe/:id',                                          recipe.show);
+api.post('/recipe',                                             recipe.create);
+api.get('/recipe/:id/image',                                    recipe.getImages);
+api.post('/recipe/:id/image',       multer().single('body'),    image.upload)
+api.delete('/recipe/:id',                                       recipe.destroy);
+
+api.get('/images/:id/:name',    image.show)
 
 // Categories
 api.get('/category',            category.categories)
--- sources/backend/src/resources/image.ts
+++ sources/backend/src/resources/image.ts
@@ -1,7 +1,8 @@
 import prisma from '../client';
 import { Request, Response } from 'express';
-import { object, string } from 'yup';
+import { object, string, ValidationError } from 'yup';
 import * as fs from 'fs';
+import { statusCodes } from './CONSTANTS';
 
 const imageSchema = object({
     base64: string().required(),
@@ -9,51 +10,69 @@ const imageSchema = object({
 });
 
 export const upload = async (req: Request, res: Response) => {
-    // console.log("REQ", req.body);
-    // console.log(req.complete);
-    // console.log(req.res)
-    // fs.readFile(req._read(), "binary", function(err, data) {
-    //     if (err) {
-    //         console.log(err);
-    //     } else {
-    //         console.log(data);
-    //     }
-    // });
-    // req.addListener("data", x => {
-    //     console.log(x.toJSON());
-    //     console.log(x.toString());
-    //     try {
-    //         fs.writeFile('test.webp', req.file.buffer, null,  err =>{
-    //             console.log(err);
-    //         });
-    //     } catch (e) {
-    //         return res.status(200).send({})
-    //     }
-    //     return;
-    // })
-    // console.log(req.body)
+    const imageFolder = `images/${req.params.id!}`;
+    const fileName = `${imageFolder}/${req.file!.originalname}`;
     try {
-        fs.writeFile('test.webp', req!.file!.buffer, null,  err =>{
+        if (!fs.existsSync("images")) {
+            fs.mkdirSync("images")
+        }
+
+        if (!fs.existsSync(imageFolder)) {
+            fs.mkdirSync(imageFolder)
+        }
+        fs.writeFile(fileName, req!.file!.buffer, null,  err => {
             console.log(err);
+            
         });
     } catch (e) {
-        return res.status(200).send({})
+        return res.status(statusCodes.InternalError).send({
+            message: "File could not be saved"
+        })
     }
+
     const image = {
-        base64: Object.keys(req.body)[0],
+        base64: fileName,
         recipeId: req.params.id!,
     }
+
     try {
         const data = await imageSchema.validate(image);
         await prisma.image.create({
-            data
+            data,
+        })
+
+        return res.status(statusCodes.Created).send({
+            status: "success",
+            message: "Image was stored",
         })
     } catch(e) {
-        //console.log(e)
+        if (e instanceof ValidationError) {
+            return res.status(statusCodes.BadRequest).send({
+                status: "error",
+                data: e.errors,
+                message: e.message
+            });
+        }
     }
 
 
-    return res.send({
-        status: 'success'
+    return res.status(statusCodes.InternalError).send({
+        status: 'error',
+        message: 'Something went wrong',
     })
+}
+
+export const show = async (req: Request, res: Response) => {
+    try {
+        const path = req.path.replace("/", "");        
+        fs.readFileSync(`${path}`) // Make sure the file exists
+        return res.status(statusCodes.Success).sendFile(`${path}`, {
+            root: process.env.REACT_APP_BACKEND_ABSOLUTE_PATH
+        })
+    } catch (e) {
+        return res.status(statusCodes.NotFound).send({
+            status: 'error',
+            message: 'Image is not present',
+        })
+    }
 }
