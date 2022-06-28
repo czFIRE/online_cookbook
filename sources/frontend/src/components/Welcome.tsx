2f4d85889080441e83db993fc8c7df90b20951ad
Petr Kadlec <xkadlec6@fi.muni.cz>
2022-06-28 16:06:24 +0200

    Feat: Final push

--- sources/frontend/src/components/Welcome.tsx
+++ sources/frontend/src/components/Welcome.tsx
@@ -14,6 +14,53 @@ export type WelcomeProps = {
 	changeViewWithData: (data: any, comp: Components) => void;
 }
 
+export const showMiniImages = async (navigation, props) => {
+	navigation('/search');
+	props.changeView();
+	let res = await axios.get(path.path.recipes).then(x => x);
+
+  
+	if (res.statusText != "OK") {
+		console.log("Error here:", res);
+		return;
+	}
+
+	let result: Promise<RecipeProps>[] = res.data.data.map(async (p, index) => {
+		console.log("p", p, index);
+		let resCategory = await axios.get(path.path.category + p.categoryId + "/name")
+		.then(x => x);
+		console.log("pp", resCategory, index)
+
+		let images = await getImages(p.id);
+		console.log("AAAAA", images);
+
+		if (resCategory.statusText != "OK") {
+			console.log("Error here:", resCategory);
+			return;
+		}
+		console.log("ppp", resCategory.data.data);
+		const res = {
+			name: p.name,
+			id: p.id,
+			portions: p.portions,
+			timeComplexity: p.timeComplexity,
+			description: p.description,
+			category: resCategory.data.data,
+			ingredients: p.ingredients,
+			steps: p.steps,
+			imageURL: images
+		};
+		return res;
+	});
+
+	console.log("res", result);
+
+	let result2 = await Promise.all(result).then((values) => values);
+	console.log("finalreisalt", result2);
+
+	props.changeViewWithData(result2, Components.SearchResult);
+}
+
 export const Welcome = (props: WelcomeProps) => {
 	const navigation = useNavigate();
   	return (
@@ -29,52 +76,7 @@ export const Welcome = (props: WelcomeProps) => {
 			<Typography color="text.primary" align='center'>
 				Try our the best recipes!
 			</Typography>
-			<Button variant="outlined" startIcon={<FastfoodIcon />} onClick={async () => {
-				navigation('/search');
-				props.changeView();
-				let res = await axios.get(path.path.recipes).then(x => x);
-
-              
-				if (res.statusText != "OK") {
-					console.log("Error here:", res);
-					return;
-				}
-
-				let result: Promise<RecipeProps>[] = res.data.data.map(async (p, index) => {
-					console.log("p", p, index);
-					let resCategory = await axios.get(path.path.category + p.categoryId + "/name")
-					.then(x => x);
-					console.log("pp", resCategory, index)
-
-					let images = await getImages(p.id);
-					console.log("AAAAA", images);
-
-					if (resCategory.statusText != "OK") {
-						console.log("Error here:", resCategory);
-						return;
-					}
-					console.log("ppp", resCategory.data.data);
-					const res = {
-						name: p.name,
-						id: p.id,
-						portions: p.portions,
-						timeComplexity: p.timeComplexity,
-						description: p.description,
-						category: resCategory.data.data,
-						ingredients: p.ingredients,
-						steps: p.steps,
-						imageURL: images
-					};
-					return res;
-				});
-
-				console.log("res", result);
-
-				let result2 = await Promise.all(result).then((values) => values);
-				console.log("finalreisalt", result2);
-
-				props.changeViewWithData(result2, Components.SearchResult);
-			}}>
+			<Button variant="outlined" startIcon={<FastfoodIcon />} onClick={async () => await showMiniImages(navigation, props)}>
 				Show recipes
 			</Button>			
 		</Grid>
